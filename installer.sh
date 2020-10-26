2fccf69e31b33aff052b6bacfbb2921a78fc0a08
gomez-crypto <71807123+gomez-crypto@users.noreply.github.com>
2020-10-26 01:03:54 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 # Ensure we are being ran as root
 if [ $(id -u) -ne 0 ]; then
@@ -9,9 +9,9 @@ fi
 # For upgrades and sanity check, remove any existing i2p.list file
 rm -f /etc/apt/sources.list.d/i2p.list
 
-# Compile the i2p ppa
+ Compile the i2p ppa
 echo "deb http://deb.i2p2.no/ unstable main" > /etc/apt/sources.list.d/i2p.list # Default config reads repos from sources.list.d
-wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O /tmp/i2p-debian-repo.key.asc # Get the latest i2p repo pubkey
+wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O /tmp/i2p-debian-repo.key.asc  Get the latest i2p repo pubkey
 apt-key add /tmp/i2p-debian-repo.key.asc # Import the key
 rm /tmp/i2p-debian-repo.key.asc # delete the temp key
 apt-get update # Update repos
@@ -21,7 +21,7 @@ then
 	apt-get install libservlet3.0-java 
 	wget http://ftp.us.debian.org/debian/pool/main/j/jetty8/libjetty8-java_8.1.16-4_all.deb
 	dpkg -i libjetty8-java_8.1.16-4_all.deb # This should succeed without error
-	apt-get install libecj-java libgetopt-java libservlet3.0-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni #installs i2p and other dependencies
+	apt-get install libecj-java libgetopt-java libservlet3.0-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni installs i2p and other dependencies
 	apt-get -f install # resolves anything else in a broken state
 fi
 
